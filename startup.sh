6f15b2307f92d6bea1527e7a3c9c7e3be33ccb56
James C Kimble <jckimble601@gmail.com>
2017-08-09 17:16:54 -0500

    docker php7

--- startup.sh
+++ startup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-/usr/local/bin/parseConfig /app/config.inc.php /etc/phpmyadmin/config.inc.php
+/usr/local/bin/ParseConfig /app/config.inc.php /etc/phpmyadmin/config.inc.php
 
 /usr/sbin/httpd -DFOREGROUND
